4db8c6a6ccb523cd7692c94d7678b842ae492b70
NATALIE_PODSEDLY <natalie.podsedly@uvm.edu>
2024-10-21 11:12:29 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+console.log(index.html)
+
+document.getElementById('submit').addEventListener('click', function() {
+    const typeOfBirdInput = document.getElementById('typeOfBird');
+    const typeOfBird = typeOfBirdInput.value;
+
+    const roommateNameInput = document.getElementById('roommateName');
+    const roommateName = roommateNameInput.value;
+
+    const nounInput = document.getElementById('noun');
+    const noun = nounInput.value;
+
+    const liquidInput = document.getElementById('liquid');
+    const liquid = liquidInput.value;
+
+    const bodyPartInput = document.getElementById('bodyPart');
+    const bodyPart = bodyPartInput.value;
+    <p> 
+    It was a cold October day. I woke up to the smell of _____ roasting in the kitchen.
+    My roomate said, "See if _____ needs a fresh ____". So I carried a glass of _____ into _____'s room.
+    When I got there, I couldn't believe my _____!
+    </p>
+});
+    
+    
+    
+    
+    
+
